902fde38d5807a51f959eb6d78c1e88ba36b36de
Nirav Shah <niravdevshah@gmail.com>
2019-07-27 15:11:18 +0530

    feat: solves the data-challenge

--- report.jsx
+++ report.jsx
@@ -4,8 +4,38 @@ var createReactClass = require('create-react-class')
 
 var rows = require('./data.json')
 
+var reduce = function(row, memo) {
+  memo.impressionTotal = (row.type === "impression" ? 1 : 0) + (memo.impressionTotal || 0);
+  memo.loadsTotal = (row.type === "load" ? 1 : 0) + (memo.loadsTotal || 0);
+  memo.displayTotal = (row.type === "display" ? 1 : 0) + (memo.displayTotal || 0);
+  memo.loadsRate = memo.loadsTotal/ memo.displayTotal;
+  memo.displayRate = memo.displayTotal /memo.loadsTotal;
+  return memo
+}
+
 module.exports = createReactClass({
   render () {
-    return <div>Report</div>
+    return (
+          <ReactPivot
+              rows={rows}
+              dimensions={[
+                {value: 'date', title: 'Date'},
+                {value: 'host', title: 'Host'}
+              ]}
+              reduce={reduce}
+              activeDimensions={['Date', 'Host']}
+              calculations={[
+                {value: 'impressionTotal', title: 'Impressions'},
+                {value: 'loadsTotal', title: 'Loads'},
+                {value: 'displayTotal', title: 'Displays'},
+                {value: 'loadsRate', title: 'Loads Rate', template: function(val, row) {
+                  return Number(val.toFixed(2)) + '%'
+                },},
+                {value: 'displayRate', title: 'Displays Rate' , template: function(val, row) {
+                  return Number(val.toFixed(2)) + '%'
+                },},
+              ]}
+          />
+    )
   }
 })
